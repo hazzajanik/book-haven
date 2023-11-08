02f29f94a01b17bbb31893bc0d392d61b46bb747
hazzajanik <hazzajanik@gmail.com>
2023-11-08 20:17:20 +0600

    modify some info in all books route

--- src/Pages/AllBooks/AllBooks.jsx
+++ src/Pages/AllBooks/AllBooks.jsx
@@ -3,7 +3,7 @@ import AllBookDetails from "./AllBookDetails";
 
 const AllBooks = () => {
     const booksInfo = useLoaderData();
-    console.log(booksInfo)
+    
     return (
         <div className="bg-rose-50">
             <h1 className="text-5xl font-semibold text-center pt-7 ">All Category Book</h1>
