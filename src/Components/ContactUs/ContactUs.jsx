6d391e549d78aa73f14005b55b0128c408719cd4
hazzajanik <hazzajanik@gmail.com>
2023-11-08 20:12:52 +0600

    added blog section name

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -2,6 +2,7 @@
 const ContactUs = () => {
     return (
         <div>
+            <h1 className="text-5xl font-semibold text-center pt-7 ">All Category Book</h1>
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-wrap -m-4 ">
@@ -18,13 +19,13 @@ const ContactUs = () => {
                                 </a>
                                 <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
                                     <span className=" mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                                        <svg className="w-4 h-4 mr-1" stroke="currentColor"  viewBox="0 0 24 24">
+                                        <svg className="w-4 h-4 mr-1" stroke="currentColor" viewBox="0 0 24 24">
                                             <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                                             <circle cx="12" cy="12" r="3"></circle>
                                         </svg>1.2K
                                     </span>
                                     <span className=" inline-flex items-center leading-none text-sm">
-                                        <svg className="w-4 h-4 mr-1" stroke="currentColor"  viewBox="0 0 24 24">
+                                        <svg className="w-4 h-4 mr-1" stroke="currentColor" viewBox="0 0 24 24">
                                             <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                                         </svg>6
                                     </span>
@@ -44,13 +45,13 @@ const ContactUs = () => {
                                 </a>
                                 <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
                                     <span className=" mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                                        <svg className="w-4 h-4 mr-1" stroke="currentColor"  viewBox="0 0 24 24">
+                                        <svg className="w-4 h-4 mr-1" stroke="currentColor" viewBox="0 0 24 24">
                                             <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                                             <circle cx="12" cy="12" r="3"></circle>
                                         </svg>1.2K
                                     </span>
                                     <span className=" inline-flex items-center leading-none text-sm">
-                                        <svg className="w-4 h-4 mr-1" stroke="currentColor"  viewBox="0 0 24 24">
+                                        <svg className="w-4 h-4 mr-1" stroke="currentColor" viewBox="0 0 24 24">
                                             <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                                         </svg>6
                                     </span>
@@ -70,13 +71,13 @@ const ContactUs = () => {
                                 </a>
                                 <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
                                     <span className=" mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                                        <svg className="w-4 h-4 mr-1" stroke="currentColor"  viewBox="0 0 24 24">
+                                        <svg className="w-4 h-4 mr-1" stroke="currentColor" viewBox="0 0 24 24">
                                             <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                                             <circle cx="12" cy="12" r="3"></circle>
                                         </svg>1.2K
                                     </span>
                                     <span className=" inline-flex items-center leading-none text-sm">
-                                        <svg className="w-4 h-4 mr-1" stroke="currentColor"  viewBox="0 0 24 24">
+                                        <svg className="w-4 h-4 mr-1" stroke="currentColor" viewBox="0 0 24 24">
                                             <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                                         </svg>6
                                     </span>
